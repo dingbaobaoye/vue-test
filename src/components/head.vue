0057077f024696fec401e249be536a26dc0e7f3e
dingbaobaoye <380612148@qq.com>
2017-10-23 19:33:41 +0800

    head_nav

--- src/components/head.vue
+++ src/components/head.vue
@@ -13,26 +13,11 @@
         </li>
         <li>
           <router-link to="service">服务支持</router-link>
-          <div class="nav_hover nav_hover_bg fr" v-show="isActive">
-            <div class="container_position">
-              <ul class="nav_ul_1 fr">
-                <li><router-link to="service">产品购买</router-link></li>
-                <li><router-link to="service">技术支持</router-link></li>
-                <li><router-link to="service">维修服务</router-link></li>
-              </ul>
-            </div>
-          </div>
+          <head-nav></head-nav>
         </li>
         <li>
           <router-link to="scenarios">商务合作</router-link>
-          <div class="nav_hover nav_hover_bg fr" v-show="isActive">
-            <div class="container_position">
-              <ul class="nav_ul_2 fr">
-                <li><router-link to="scenarios">市场前景</router-link></li>
-                <li><router-link to="scenarios">联系我们</router-link></li>
-              </ul>
-            </div>
-          </div>
+          <head-nav></head-nav>
         </li>
         <li>
           <router-link to="brand">智慧嘉孚</router-link>
@@ -46,13 +31,15 @@
 </template>
 
 <script>
-    export default {
-        data() {
-          return {
-            isActive : true
-          }
-        }
+  import Vue from 'vue';
+  import headNav from '../components/head_nav';
+  export default {
+    name: 'head',
+    components: {
+      headNav
     }
+  }
+
 </script>
 
 <style scoped>
@@ -64,10 +51,5 @@
   .nav>li>a{display:block;height:100%;padding:0 32px;}
   .nav>li a:hover{color:#0080db;}
   .nav>li.active>a{color:#0080db;}
-  .nav_hover_bg {height: 50px;background: rgba(255,255,255,0.8);position: absolute;width: 100%;left: 0;z-index: 1;}
-  .container_position {position: relative;width: 1280px;margin: 0 auto;}
-  .nav_ul_1 {position: absolute;z-index: 99;top: 0;right: 335px;}
-  .nav_ul_2 {position: absolute;z-index: 99;top: 0;right: 245px;}
-  .nav_hover ul li {float: left;height: 50px;line-height: 50px;font-size: 15px;width: 85px;text-align: center;}
-  .nav_hover ul li a{font-size:13px;}
+
 </style>
---
+++ src/components/head_nav.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="nav_hover nav_hover_bg fr" v-show="isActive">
+      <div class="container_position">
+        <ul v-bind:class="[isFirst ? 'nav_ul_1' : 'nav_ul_2' , 'fr'] ">
+          <li><router-link to="scenarios">市场前景</router-link></li>
+          <li><router-link to="scenarios">联系我们</router-link></li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isActive : true,
+        isFirst : false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .nav_hover_bg {height: 50px;background: rgba(255,255,255,0.8);position: absolute;width: 100%;left: 0;z-index: 1;}
+  .container_position {position: relative;width: 1280px;margin: 0 auto;}
+  .nav_ul_1 {position: absolute;z-index: 99;top: 0;right: 335px;}
+  .nav_ul_2 {position: absolute;z-index: 99;top: 0;right: 245px;}
+  .nav_hover ul li {float: left;height: 50px;line-height: 50px;font-size: 15px;width: 85px;text-align: center;}
+  .nav_hover ul li a{font-size:13px;}
+  .nav>li a:hover{color:#0080db;}
+  .nav>li.active>a{color:#0080db;}
+</style>
